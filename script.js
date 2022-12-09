5cadff4a6b968d2e1930d4efdabc24b1d1ed29c4
jasmin <yase0236@stud.kea.dk>
2022-12-09 21:41:44 +0100

    back to vanilla js - styling

---
+++ script.js
@@ -0,0 +1,23 @@
+// "use strict";
+
+// //starting the page up
+// document.addEventListener("DOMContentLoaded", init);
+
+// //==================all my variables==================
+// let allStages = [];
+// let allDays = [];
+
+// const scheduleURL = "http://localhost:8080/schedule";
+// // const bandsURL = "http://localhost:8080/bands";
+
+// //==================starting off the page==================
+// function init() {
+//   loadSchedule();
+// }
+
+// async function loadSchedule() {
+//   const response = await fetch(scheduleURL);
+//   const scheduleData = await response.json();
+
+//   prepareSchedule(scheduleData);
+// }
