762cdcadf5e64cb1087d4e94b1b9eee203f1cd8c
jasmin <yase0236@stud.kea.dk>
2022-12-10 12:01:16 +0100

    fetching data

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-// "use strict";
+"use strict";
 
-// //starting the page up
-// document.addEventListener("DOMContentLoaded", init);
+//starting the page up
+document.addEventListener("DOMContentLoaded", init);
 
-// //==================all my variables==================
-// let allStages = [];
-// let allDays = [];
+//==================all my variables==================
+let allStages = [];
+let allDays = [];
 
 // const scheduleURL = "http://localhost:8080/schedule";
-// // const bandsURL = "http://localhost:8080/bands";
+// const bandsURL = "http://localhost:8080/bands";
 
-// //==================starting off the page==================
-// function init() {
-//   loadSchedule();
-// }
+//==================starting off the page==================
+function init() {
+  loadSchedule();
+}
 
-// async function loadSchedule() {
-//   const response = await fetch(scheduleURL);
-//   const scheduleData = await response.json();
+async function loadSchedule() {
+  //   const response = await fetch(scheduleURL);
+  //   const scheduleData = await response.json();
+  //   displayList();
 
-//   prepareSchedule(scheduleData);
-// }
+  fetch("http://localhost:8080/schedule")
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+  displayList();
+}
+function displayList() {
+  document.querySelector("#scheduleList").innerHTML = "";
+}
