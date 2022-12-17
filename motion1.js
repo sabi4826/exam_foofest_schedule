7cba31f8cd39ad927f943229be797217c23c8bf3
jasmin <yase0236@stud.kea.dk>
2022-12-17 23:37:43 +0100

    fixing the motion one animation

--- motion1.js
+++ motion1.js
@@ -1,17 +1,12 @@
 "use strict";
-
+// import the animation from motion one website
 import { inView, animate, scroll } from "https://cdn.skypack.dev/motion";
 
-/* scrolling animations */
-//PROGRESS BAR
+/* scrolling bar animation */
+//this is the progress bar - it will show a line on the left side of the screen (before scrolling)
 scroll(animate(".progress", { scaleY: [0, 1] }));
 
-// Selector
-inView(".highlight h2", (info) => {
-  const controls = animate(info.target, { opacity: [0, 1] }, { duration: 2 }, { repeat: 0 });
-});
-
-//animate article
-inView(".article", (info) => {
+//ensures that the scroll bar is a bit more smooth
+inView((info) => {
   const controls = animate(info.target, { opacity: [0, 1] }, { duration: 2 });
 });
