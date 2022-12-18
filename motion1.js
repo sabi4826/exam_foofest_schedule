a818fbbc0b869de80d993d0053a594469cf045f0
jasmin <yase0236@stud.kea.dk>
2022-12-18 13:13:56 +0100

    adding motion one to other files

--- motion1.js
+++ motion1.js
@@ -1,6 +1,6 @@
 "use strict";
 // import the animation from motion one website
-import { inView, animate, scroll } from "https://cdn.skypack.dev/motion";
+import { animate, scroll } from "https://cdn.skypack.dev/motion";
 
 /* scrolling bar animation */
 //this is the progress bar - it will show a line on the left side of the screen (before scrolling)
