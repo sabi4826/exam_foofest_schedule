ebbf3e67881c801676e1e9db1cb0d884157207c8
jasmin <yase0236@stud.kea.dk>
2022-12-20 21:58:07 +0100

    motiuon1js

--- motion1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import the animation from motion one website
-import { animate, scroll } from "https://cdn.skypack.dev/motion";
-
-/* scrolling bar animation */
-//this is the progress bar - it will show a line on the left side of the screen (before scrolling)
-scroll(animate(".progress", { scaleY: [0, 1] }));
